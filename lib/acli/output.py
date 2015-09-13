4464ed2eb120194500bb05f0fc5a705f2089a829
Jon Hadfield <jon@lessknown.co.uk>
2015-09-13 18:04:40 +0100

    add instance info.

--- lib/acli/output.py
+++ lib/acli/output.py
@@ -27,7 +27,7 @@ def output_ec2_list(output_media=None, instances=None):
             instance_profile_out = '-'
             instance_name = get_ec2_instance_name_tag(ec2_instance=instance[0])
             if instance_profile:
-                instance_profile_out = instance_profile.get('arn').split('/')[-1]
+                instance_profile_out = short_instance_profile(instance_profile)
             table_data.append([instance_id,
                                instance_name,
                                instance_state,
@@ -40,17 +40,35 @@ def output_ec2_list(output_media=None, instances=None):
         print(table.table)
 
 
+def dash_if_none(item=None):
+    return item if item else '-'
+
+def short_instance_profile(instance_profile):
+    return instance_profile.get('arn').split('/')[-1]
+
+
 def output_ec2_info(output_media=None, instance=None):
     if output_media == 'console':
         table_data = [['id', instance.id]]
-        table_data.append(['',''])
-        table_data.append(['',''])
-        table_data.append(['',''])
-        table_data.append(['',''])
-        table_data.append(['',''])
-        table_data.append(['',''])
-        table_data.append(['',''])
-        table_data.append(['',''])
+        table_data.append(['groups', str(instance.groups)])
+        table_data.append(['public dns name', dash_if_none(instance.public_dns_name)])
+        table_data.append(['private dns name', dash_if_none(instance.private_dns_name)])
+        table_data.append(['state', dash_if_none(instance.state)])
+        table_data.append(['previous state', dash_if_none(instance.previous_state)])
+        table_data.append(['key name', dash_if_none(instance.key_name)])
+        table_data.append(['instance type', dash_if_none(instance.instance_type)])
+        table_data.append(['launch time', dash_if_none(instance.launch_time)])
+        table_data.append(['image id', dash_if_none(instance.image_id)])
+        table_data.append(['placement', dash_if_none(instance.placement)])
+        table_data.append(['monitored', dash_if_none(str(instance.monitored))])
+        table_data.append(['subnet id', dash_if_none(instance.subnet_id)])
+        table_data.append(['vpc id', dash_if_none(instance.vpc_id)])
+        table_data.append(['root device type', dash_if_none(instance.root_device_type)])
+        table_data.append(['state reason', dash_if_none(instance.state_reason)])
+        table_data.append(['interfaces', dash_if_none(str(instance.interfaces))])
+        table_data.append(['ebs optimized', dash_if_none(instance.ebs_optimized)])
+        table_data.append(['instance profile', dash_if_none(short_instance_profile(instance.instance_profile))])
+
         #for instance in instances:
         #    instance_id = instance[0].id
         #    instance_state = instance[0].state
