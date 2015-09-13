a0dfb1ce1a72880da34ad817c8021e54e2ce0e5d
Jon Hadfield <jon@lessknown.co.uk>
2015-09-13 16:09:02 +0100

    add fields.

--- lib/acli/output.py
+++ lib/acli/output.py
@@ -4,13 +4,24 @@ from terminaltables import AsciiTable
 
 def output_ec2(output_type=None, instances=None):
     if output_type == 'console':
-        heading = ['id', 'state']
+        heading = ['id', 'state', 'type', 'image', 'public ip', 'private ip']
         table_data = [heading]
 
         for instance in instances:
             instance_id = instance[0].id
             instance_state = instance[0].state
-            table_data.append([instance_id, instance_state])
+            instance_type = instance[0].instance_type
+            image_id = instance[0].image_id
+            public_ip = instance[0].ip_address
+            private_ip = instance[0].private_ip_address
+
+            table_data.append([instance_id,
+                               instance_state,
+                               instance_type,
+                               image_id,
+                               public_ip if public_ip else '-',
+                               private_ip if private_ip else '-'])
+
         table = AsciiTable(table_data)
         print(table.table)
 
@@ -26,7 +37,3 @@ def output_elb(output_type=None, elbs=None):
             table_data.append([elb_id, elb_name])
         table = AsciiTable(table_data)
         print(table.table)
-
-# def console_table(collection_type, collection_data):
-#    pass
-
