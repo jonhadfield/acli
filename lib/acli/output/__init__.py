5ad7b35994ce02c6bdf97cc551d10f8a83df7718
Jon Hadfield <jon@lessknown.co.uk>
2015-09-19 16:40:55 +0100

    start shifting to boto3 for ec2.

--- lib/acli/output/__init__.py
+++ lib/acli/output/__init__.py
@@ -16,13 +16,12 @@ def dash_if_none(item=None):
     return item if item else '-'
 
 
-def output_tags(tags):
+def get_tags(tags):
     tag_list = []
-    for tag_name, tag_value in tags.iteritems():
-        tag_list.append("{0}:{1}".format(tag_name, tag_value))
+    for tag in tags:
+        tag_list.append("{0}:{1}".format(tag.get('Key'), tag.get('Value')))
     if tag_list:
-        return ",\n".join(tag_list)
-    return "-"
+        return ", ".join(tag_list)
 
 
 def output_name_tag(tags):
--- lib/acli/output/ec2.py
+++ lib/acli/output/ec2.py
@@ -1,15 +1,56 @@
 from __future__ import (absolute_import, print_function)
-from acli.output import (output_ascii_table, dash_if_none, output_tags)
+from acli.output import (output_ascii_table, dash_if_none, get_tags)
 from boto.resultset import ResultSet
 
 
 def get_ec2_instance_name_tag(ec2_instance=None,
                               max_length=40):
-    instance_name = ec2_instance.tags.get('Name', '-')
-    if len(instance_name) > max_length:
-        return "{0}...".format(instance_name[:max_length-3])
-    else:
-        return instance_name
+    for tag in ec2_instance.tags:
+        if tag.get('Key') == "Name":
+            name_tag = tag.get('Value', None)
+            if name_tag:
+                if len(name_tag) > max_length:
+                    return "{0}...".format(name_tag[:max_length-3])
+                else:
+                    return name_tag
+
+
+def get_interfaces(interfaces):
+    ret = []
+    for interface in interfaces:
+        ret.append("{0}\n".format(interface.get('NetworkInterfaceId')))
+        ret.append(" Attachment:\n")
+        for akey, avalue in interface.get('Attachment').iteritems():
+                ret.append("  {}:{}\n".format(str(akey), str(avalue)))
+        ret.append(" Private IP Addresses:\n")
+        for private_ip_address in interface.get('PrivateIpAddresses'):
+                for pkey, pvalue in private_ip_address.iteritems():
+                    print(pkey)
+                    if pkey == "Association":
+                        ret.append("  Association:\n")
+                        for qqkey, qqvalue in pvalue.iteritems():
+                            ret.append("   {}:{}\n".format(qqkey, qqvalue))
+                    else:
+                        ret.append("  {}:{}\n".format(str(pkey), str(pvalue)))
+        ret.append(" Security Groups:\n")
+        for group in interface.get('Groups'):
+            for gkey, gvalue in group.iteritems():
+                ret.append("  {}:{}\n".format(str(gkey), str(gvalue)))
+        for key, value in interface.iteritems():
+            if str(key) not in ("Attachment", "NetworkInterfaceId",
+                                "PrivateIpAddresses", "Groups", "Association",
+                                "PrivateIpAddress"):
+                print(str(key))
+                ret.append(" {}:{}\n".format(str(key), str(value)))
+    return ("".join(ret)).rstrip()
+
+
+def get_placement_details(placement):
+    ret = []
+    for key, value in placement.iteritems():
+        ret.append("{0}:{1}".format(key, value))
+    if ret:
+        return "\n".join(ret)
 
 
 def output_ec2_list(output_media=None, instances=None):
@@ -20,20 +61,20 @@ def output_ec2_list(output_media=None, instances=None):
         for instance in instances:
             if isinstance(instance, ResultSet):
                 instance = instance[0]
-            instance_id = instance.id
-            instance_state = instance.state
-            instance_type = instance.instance_type
-            image_id = instance.image_id
-            public_ip = instance.ip_address
+            instance_id = instance.instance_id
+            instance_state = dash_if_none(str(instance.state))
+            instance_type = dash_if_none(str(instance.instance_type))
+            image_id = dash_if_none(instance.image_id)
+            public_ip = dash_if_none(instance.public_ip_address)
             private_ip = instance.private_ip_address
-            instance_name = get_ec2_instance_name_tag(ec2_instance=instance)
+            instance_name = dash_if_none(get_ec2_instance_name_tag(ec2_instance=instance))
             td.append([instance_id,
                        instance_name,
                        instance_state,
                        instance_type,
                        image_id,
-                       public_ip if public_ip else '-',
-                       private_ip if private_ip else '-'])
+                       public_ip,
+                       private_ip])
         output_ascii_table(table_title="EC2 Instances",
                            table_data=td,
                            inner_heading_row_border=True)
@@ -59,28 +100,29 @@ def output_ec2_info(output_media=None, instance=None):
     if output_media == 'console':
         td = list()
         td.append(['id', instance.id])
-        td.append(['name', instance.tags.get('Name', '-')])
-        td.append(['groups', get_sec_group_names(instance.groups)])
-        td.append(['public ip', dash_if_none(instance.ip_address)])
+        td.append(['name', dash_if_none(get_ec2_instance_name_tag(ec2_instance=instance))])
+        td.append(['groups', str(instance.security_groups)])
+        td.append(['public ip', dash_if_none(instance.public_ip_address)])
         td.append(['public dns name', dash_if_none(instance.public_dns_name)])
         td.append(['private ip', dash_if_none(instance.private_ip_address)])
         td.append(['private dns name', dash_if_none(instance.private_dns_name)])
-        td.append(['state', dash_if_none(instance.state)])
-        td.append(['previous state', dash_if_none(instance.previous_state)])
+        td.append(['state', dash_if_none(instance.state.get('Name', None))])
         td.append(['key name', dash_if_none(instance.key_name)])
         td.append(['instance type', dash_if_none(instance.instance_type)])
-        td.append(['launch time', dash_if_none(instance.launch_time)])
+        td.append(['launch time', str(instance.launch_time)])
         td.append(['image id', dash_if_none(instance.image_id)])
-        td.append(['placement', dash_if_none(instance.placement)])
-        td.append(['monitored', dash_if_none(str(instance.monitored))])
+        td.append(['placement', get_placement_details(instance.placement)])
+        td.append(['monitored', dash_if_none(instance.monitoring.get('State'))])
         td.append(['subnet id', dash_if_none(instance.subnet_id)])
         td.append(['vpc id', dash_if_none(instance.vpc_id)])
         td.append(['root device type', dash_if_none(instance.root_device_type)])
         td.append(['state reason', dash_if_none(instance.state_reason)])
-        td.append(['interfaces', dash_if_none(get_interface_ids(instance.interfaces))])
+        td.append(['state transition reason', dash_if_none(instance.state_transition_reason)])
+
         td.append(['ebs optimized', dash_if_none(instance.ebs_optimized)])
-        td.append(['instance profile', dash_if_none(short_instance_profile(instance.instance_profile))])
-        td.append(['tags', output_tags(instance.tags)])
+        td.append(['instance profile', str(instance.iam_instance_profile.get('Arn', None))])
+        #td.append(['tags', get_tags(instance.tags)])
+        td.append(['interfaces', get_interfaces(instance.network_interfaces)])
         output_ascii_table(table_title="Instance Info",
                            table_data=td)
     exit(0)
