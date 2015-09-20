7bac48e4371618a9ece441c951033a8f9d53280b
Jon Hadfield <jon@lessknown.co.uk>
2015-09-20 15:37:30 +0100

    get account info.

--- lib/acli/services/account.py
+++ lib/acli/services/account.py
@@ -1,6 +1,13 @@
 from __future__ import (absolute_import, print_function)
-import boto
+from boto3.session import Session
+from boto3 import client
 from acli.output.account import output_account_info
+
+
+def get_boto3_session(aws_config):
+    return Session(region_name=aws_config.region,
+                   aws_access_key_id=aws_config.access_key_id,
+                   aws_secret_access_key=aws_config.secret_access_key)
 
 
 def get_iam_conn(aws_config):
@@ -18,7 +25,10 @@ def get_account_id(iam_conn):
 
 
 def account_info(aws_config):
-    iam_conn = get_iam_conn(aws_config)
+    session = get_boto3_session(aws_config)
+    iam_client = session.client('iam')
+    aliases = iam_client.list_account_aliases().get('AccountAliases')
+    account_id = iam_client.get_user().get('User').get('Arn').split(':')[4]
     output_account_info(output_media='console',
-                        account_id=get_account_id(iam_conn),
-                        account_aliases=get_account_aliases(iam_conn))
+                        account_id=account_id,
+                        account_aliases=aliases)
