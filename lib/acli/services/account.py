22c837d340a30e3541350803694c20825b3a04de
Jon Hadfield <jon@lessknown.co.uk>
2015-09-20 15:43:01 +0100

    remove unused code.

--- lib/acli/services/account.py
+++ lib/acli/services/account.py
@@ -1,6 +1,5 @@
 from __future__ import (absolute_import, print_function)
 from boto3.session import Session
-from boto3 import client
 from acli.output.account import output_account_info
 
 
@@ -8,20 +7,6 @@ def get_boto3_session(aws_config):
     return Session(region_name=aws_config.region,
                    aws_access_key_id=aws_config.access_key_id,
                    aws_secret_access_key=aws_config.secret_access_key)
-
-
-def get_iam_conn(aws_config):
-    return boto.connect_iam(aws_access_key_id=aws_config.access_key_id,
-                            aws_secret_access_key=aws_config.secret_access_key)
-
-
-def get_account_aliases(iam_conn):
-    igaa = iam_conn.get_account_alias()
-    return igaa.list_account_aliases_response.list_account_aliases_result.account_aliases
-
-
-def get_account_id(iam_conn):
-    return iam_conn.get_user()['get_user_response']['get_user_result']['user']['arn'].split(':')[4]
 
 
 def account_info(aws_config):
