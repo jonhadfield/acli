0cb7f9c41c7ae0a7f487188721f56adf2ff9999d
Jon Hadfield <jon@lessknown.co.uk>
2015-10-04 13:54:03 +0100

    add type hints.

--- lib/acli/services/route53.py
+++ lib/acli/services/route53.py
@@ -6,12 +6,18 @@ import botocore.exceptions
 
 
 def get_boto3_session(aws_config):
+    """
+    @type aws_config: Config
+    """
     return Session(region_name=aws_config.region,
                    aws_access_key_id=aws_config.access_key_id,
                    aws_secret_access_key=aws_config.secret_access_key)
 
 
 def route53_list(aws_config=None):
+    """
+    @type aws_config: Config
+    """
     session = get_boto3_session(aws_config)
     conn = session.client('route53')
     output_route53_list(output_media='console', zones=conn.list_hosted_zones())
