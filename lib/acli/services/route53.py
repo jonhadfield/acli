5555b04c65f6d585794d48ed115ea9496d52fbdc
Jon Hadfield <jon@lessknown.co.uk>
2015-10-04 13:53:27 +0100

    tidy up exceptions.

--- lib/acli/services/route53.py
+++ lib/acli/services/route53.py
@@ -2,7 +2,8 @@
 from __future__ import (absolute_import, print_function, unicode_literals)
 from boto3.session import Session
 from acli.output.route53 import (output_route53_list, output_route53_info)
-import botocore
+import botocore.exceptions
+
 
 def get_boto3_session(aws_config):
     return Session(region_name=aws_config.region,
@@ -25,13 +26,12 @@ def route53_info(aws_config=None, zone_id=None):
     conn = session.client('route53')
     try:
         hosted_zone = conn.get_hosted_zone(Id=zone_id)
-    except botocore.exceptions.ClientError:
-        exit("Cannot request hosted zone: {0}".format(zone_id))
-    record_sets = conn.list_resource_record_sets(HostedZoneId=zone_id)
-    try:
+        record_sets = conn.list_resource_record_sets(HostedZoneId=zone_id)
         if hosted_zone['HostedZone']['Id']:
             output_route53_info(output_media='console',
                                 zone=hosted_zone,
                                 record_sets=record_sets)
     except AttributeError:
         exit("Cannot find hosted zone: {0}".format(zone_id))
+    except botocore.exceptions.ClientError:
+        exit("Cannot request hosted zone: {0}".format(zone_id))
