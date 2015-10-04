c38340bfdbf57a4dd1b25b9b5fb195650751573f
Jon Hadfield <jon@lessknown.co.uk>
2015-10-04 13:51:16 +0100

    capture request error for hostedzone.

--- lib/acli/services/route53.py
+++ lib/acli/services/route53.py
@@ -2,7 +2,7 @@
 from __future__ import (absolute_import, print_function, unicode_literals)
 from boto3.session import Session
 from acli.output.route53 import (output_route53_list, output_route53_info)
-
+import botocore
 
 def get_boto3_session(aws_config):
     return Session(region_name=aws_config.region,
@@ -23,7 +23,10 @@ def route53_info(aws_config=None, zone_id=None):
     """
     session = get_boto3_session(aws_config)
     conn = session.client('route53')
-    hosted_zone = conn.get_hosted_zone(Id=zone_id)
+    try:
+        hosted_zone = conn.get_hosted_zone(Id=zone_id)
+    except botocore.exceptions.ClientError:
+        exit("Cannot request hosted zone: {0}".format(zone_id))
     record_sets = conn.list_resource_record_sets(HostedZoneId=zone_id)
     try:
         if hosted_zone['HostedZone']['Id']:
