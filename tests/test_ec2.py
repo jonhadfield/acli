38fd2bb71782f4b881ded25973a0e6baeaa82f5c
Jon Hadfield <jon@lessknown.co.uk>
2015-10-14 11:33:39 +0100

    add test for ec2 service info.

--- tests/test_ec2.py
+++ tests/test_ec2.py
@@ -1,5 +1,7 @@
-from __future__ import (absolute_import, print_function)
+from __future__ import (absolute_import, print_function, unicode_literals)
 from acli.output.ec2 import (output_ec2_list, output_ec2_info)
+from acli.services.ec2 import (ec2_list, ec2_info)
+from acli.config import Config
 from moto import mock_ec2
 import pytest
 from boto3.session import Session
@@ -20,16 +22,36 @@ def ec2_instances():
             Tags=[{'Key': 'Name', 'Value': 'Bob'}])
     ec2_resource = session.resource('ec2')
     all_instances = ec2_resource.instances.all()
+    print(list(all_instances)[0].id)
     yield all_instances
     mock.stop()
 
 
-def test_ec2_list(ec2_instances):
+config = Config(cli_args={'--region': 'eu-west-1',
+                          '--access_key_id': 'AKIAIOSFODNN7EXAMPLE',
+                          '--secret_access_key': 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'})
+
+@mock_ec2
+def test_ec2_list_service():
+    with pytest.raises(SystemExit):
+        assert ec2_list(aws_config=config)
+
+
+def test_ec2_info_service(ec2_instances):
+    config = Config(cli_args={'--region': 'eu-west-1',
+                              '--access_key_id': 'AKIAIOSFODNN7EXAMPLE',
+                              '--secret_access_key': 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'})
+    with pytest.raises(SystemExit):
+        print(list(ec2_instances))
+        assert ec2_info(aws_config=config, instance_id=list(ec2_instances)[0].id)
+
+
+def test_ec2_list_output(ec2_instances):
     with pytest.raises(SystemExit):
         assert output_ec2_list(output_media='console', instances=ec2_instances)
 
 
-def test_ec2_info(ec2_instances):
+def test_ec2_output(ec2_instances):
     with pytest.raises(SystemExit):
         instance = list(ec2_instances)[0]
         print(dir(instance))
