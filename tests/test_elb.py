c1bf53c5c278cafa3b1c070f8a232d5820dcb7a4
Jon Hadfield <jon@lessknown.co.uk>
2015-10-16 19:50:44 +0100

    add elb list test.

---
+++ tests/test_elb.py
@@ -0,0 +1,42 @@
+from __future__ import (absolute_import, print_function, unicode_literals)
+from acli.output.elb import (output_elb_info, output_elbs)
+from acli.services.elb import (elb_info, elb_list)
+from acli.config import Config
+from moto import mock_elb
+import pytest
+from boto3.session import Session
+session = Session(region_name="eu-west-1")
+
+
+@pytest.yield_fixture(scope='function')
+def elb_instances():
+    """ELB mock service"""
+    mock = mock_elb()
+    mock.start()
+    client = session.client('elb')
+    zones = ['eu-west-1a', 'eu-west-1b']
+    listeners = [{
+            'Protocol': 'string',
+            'LoadBalancerPort': 123,
+            'InstanceProtocol': 'string',
+            'InstancePort': 123,
+            'SSLCertificateId': 'string'}]
+    client.create_load_balancer(LoadBalancerName='my-lb', AvailabilityZones=zones, Listeners=listeners)
+    yield client.describe_load_balancers()
+    mock.stop()
+
+
+config = Config(cli_args={'--region': 'eu-west-1',
+                          '--access_key_id': 'AKIAIOSFODNN7EXAMPLE',
+                          '--secret_access_key': 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'})
+
+
+def test_elb_list_service(elb_instances):
+    with pytest.raises(SystemExit):
+        assert elb_list(aws_config=config)
+
+
+# def test_elb_info_service(ec2_instances):
+#    with pytest.raises(SystemExit):
+#        assert elb_info(aws_config=config, elb_name=list(ec2_instances)[0].id)
+
