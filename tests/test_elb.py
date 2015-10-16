cc0e08681649519d420d8ec0c2072ade104eaea2
Jon Hadfield <jon@lessknown.co.uk>
2015-10-16 22:22:46 +0100

    remove comment.

--- tests/test_elb.py
+++ tests/test_elb.py
@@ -39,8 +39,3 @@ def test_elb_list_service(elb_instances):
 def test_elb_info_service(elb_instances):
     with pytest.raises(SystemExit):
         assert elb_info(aws_config=config, elb_name='my-lb')
-
-# def test_elb_info_service(ec2_instances):
-#    with pytest.raises(SystemExit):
-#        assert elb_info(aws_config=config, elb_name=list(ec2_instances)[0].id)
-
