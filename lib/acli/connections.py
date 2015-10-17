64c91cd99bdd55ca31737eb49f444e05b908af26
Jon Hadfield <jon@lessknown.co.uk>
2015-10-17 17:12:05 +0100

    check for credentials.

--- lib/acli/connections.py
+++ lib/acli/connections.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import (absolute_import, print_function, unicode_literals)
 from boto3.session import Session
+from botocore.exceptions import ClientError, PartialCredentialsError, NoCredentialsError
 
 
 def get_boto3_session(aws_config):
     """
     @type aws_config: Config
     """
-    return Session(region_name=aws_config.region,
-                   aws_access_key_id=aws_config.access_key_id,
-                   aws_secret_access_key=aws_config.secret_access_key)
-
-
-#def get_elb_conn(aws_config):
-#    """
-#    @type aws_config: Config
-#    """
-#    return boto.ec2.elb.connect_to_region(aws_access_key_id=aws_config.access_key_id,
-#                                          aws_secret_access_key=aws_config.secret_access_key,
-#                                          region_name=aws_config.region)
-
+    session = Session(region_name=aws_config.region,
+                      aws_access_key_id=aws_config.access_key_id,
+                      aws_secret_access_key=aws_config.secret_access_key)
+    try:
+        iam_client = session.client('iam')
+        iam_client.list_account_aliases()
+        return session
+    except ClientError:
+        exit('Please check credentials.')
+    except NoCredentialsError:
+        exit('Credentials not found.')
+    except PartialCredentialsError:
+        exit('Credentials are incomplete.')
