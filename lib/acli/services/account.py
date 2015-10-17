7adfbb6027d5bda8d924699b30c92e302e98d168
Jon Hadfield <jon@lessknown.co.uk>
2015-10-17 17:56:39 +0100

    add credentials check for accounts.

--- lib/acli/services/account.py
+++ lib/acli/services/account.py
@@ -2,6 +2,7 @@
 from __future__ import (absolute_import, print_function, unicode_literals)
 from acli.output.account import output_account_info
 from acli.connections import get_boto3_session
+from botocore.exceptions import NoCredentialsError
 
 
 def account_info(aws_config):
@@ -10,8 +11,13 @@ def account_info(aws_config):
     """
     session = get_boto3_session(aws_config)
     iam_client = session.client('iam')
-    aliases = iam_client.list_account_aliases().get('AccountAliases')
-    account_id = iam_client.get_user().get('User').get('Arn').split(':')[4]
-    output_account_info(output_media='console',
-                        account_id=account_id,
-                        account_aliases=aliases)
+    try:
+        aliases = iam_client.list_account_aliases().get('AccountAliases')
+        account_id = iam_client.get_user().get('User').get('Arn').split(':')[4]
+        output_account_info(output_media='console',
+                            account_id=account_id,
+                            account_aliases=aliases)
+    except NoCredentialsError:
+        exit('No credentials were found.')
+    except Exception as e:
+        exit('Unhandled exception: {0}'.format(e))
