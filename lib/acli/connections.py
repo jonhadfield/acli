d448b3354bb714c58accc5c8bb6d26be1ce6ef33
Jon Hadfield <jon@lessknown.co.uk>
2015-10-17 17:52:10 +0100

    undo credentials check.

--- lib/acli/connections.py
+++ lib/acli/connections.py
@@ -1,23 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import (absolute_import, print_function, unicode_literals)
 from boto3.session import Session
-from botocore.exceptions import ClientError, PartialCredentialsError, NoCredentialsError
 
 
 def get_boto3_session(aws_config):
     """
     @type aws_config: Config
     """
-    session = Session(region_name=aws_config.region,
-                      aws_access_key_id=aws_config.access_key_id,
-                      aws_secret_access_key=aws_config.secret_access_key)
-    try:
-        iam_client = session.client('iam')
-        iam_client.list_account_aliases()
-        return session
-    except ClientError:
-        exit('Please check credentials.')
-    except NoCredentialsError:
-        exit('Credentials not found.')
-    except PartialCredentialsError:
-        exit('Credentials are incomplete.')
+    return Session(region_name=aws_config.region,
+                   aws_access_key_id=aws_config.access_key_id,
+                   aws_secret_access_key=aws_config.secret_access_key)
