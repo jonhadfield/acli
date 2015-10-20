0d255cfd82afe699048f7db4286a19db49217c1d
Jon Hadfield <jon@lessknown.co.uk>
2015-10-20 14:56:54 +0100

    replace resource with LLC.

--- lib/acli/services/secgroup.py
+++ lib/acli/services/secgroup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import (absolute_import, print_function, unicode_literals)
 from acli.output.secgroup import (output_secgroup_list, output_secgroup_info)
-from acli.connections import get_boto3_session, get_client
+from acli.connections import get_client
 
 
 def secgroup_list(aws_config=None):
@@ -19,9 +19,10 @@ def secgroup_info(aws_config=None, secgroup_id=None):
     @type secgroup_id: unicode
     """
     ec2_client = get_client(client_type='ec2', config=aws_config)
-    secgroups = ec2_client.describe_security_groups(GroupIds=[secgroup_id])
-    secgroup = secgroups.get('SecurityGroups')[0]
-    if secgroup.get('GroupId'):
+    result = ec2_client.describe_security_groups(GroupIds=[secgroup_id])
+    secgroups = result.get('SecurityGroups', None)
+    if secgroups:
+        secgroup = secgroups[0]
         output_secgroup_info(output_media='console', secgroup=secgroup)
     else:
         exit("Cannot find security group: {0}.".format(secgroup_id))
