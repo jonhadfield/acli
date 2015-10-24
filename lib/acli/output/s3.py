ec27f1faa0a353f1d1e4e0473cc7a2508f0e903b
Jon Hadfield <jon@lessknown.co.uk>
2015-10-24 14:03:11 +0100

    tidy up s3 list output.

--- lib/acli/output/s3.py
+++ lib/acli/output/s3.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import (absolute_import, print_function, unicode_literals)
-from acli.output import (output_ascii_table, dash_if_none)
+from acli.output import (output_ascii_table)
 import re
 
 
 def output_s3_list(output_media=None, buckets=None, bucket_name=None, objects=None, folders=None, item=None):
     """
     @type output_media: unicode
-    @type buckets: list
-    @type objects: list
-    @type bucket_name: list
+    @type buckets: dict
+    @type objects: dict
+    @type bucket_name: unicode
     """
     to_remove_len = len(re.sub(bucket_name, '', item))-1
     if buckets:
@@ -24,74 +24,26 @@ def output_s3_list(output_media=None, buckets=None, bucket_name=None, objects=No
                            inner_heading_row_border=True)
     if any((objects, folders)):
         td = list()
-        td.append(['item', 'last modified'])
+        td.append(['item', 'size (bytes)', 'last modified (UTC)', 'class', 'etag'])
         if folders:
             for folder in folders:
-                td.append([folder.get('Prefix')[to_remove_len:], '-'])
-        table_title = str()
-        if item:
-            table_title = item
-        print("OBJECTS: {}".format(objects))
-        print("BUCKET NAME: {0}".format(bucket_name))
+                td.append([folder.get('Prefix')[to_remove_len:], '-', '-'])
         object_list = objects.get('Contents', None)
         if objects.get('Contents', None):
             sorted_object_list = sorted(object_list, key=lambda k: ['Key'])
-            print('------')
             for index, an_object in enumerate(sorted_object_list):
-                print(an_object)
-                #url = 'abcdc.com'
-                #url = re.sub('\.com$', '', url)
-                print('item minus bucket = {}'.format(re.sub(bucket_name, '', item)[1:]))
-
                 an_object_key = an_object.get('Key')[to_remove_len:]
-                #an_object_key = an_object_key.replace(an_object_key, '/'+bucket_name, '')
-            #if index == 0:
-            #    table_title = an_object.get('Key')
-            #else:
-            #    print(an_object)
-                #if not an_object.get('Key') in :
-                #if an_object_key != '/':
                 if an_object_key:
-                    td.append([an_object_key, str(an_object.get('LastModified'))])
+                    td.append([an_object_key,
+                               str(an_object.get('Size')),
+                               str(an_object.get('LastModified').replace(tzinfo=None)),
+                               str(an_object.get('StorageClass')),
+                               # str(an_object.get('Owner')),
+                               str(an_object.get('ETag')[1:-1])])
+
         else:
             print("NO OBJECTS TO OUTPUT :(")
         output_ascii_table(table_title=item,
                            table_data=td,
                            inner_heading_row_border=True)
-        #print(bucket_name)
-        #print(objects)
-        print('done with objects')
-        # if item and '\\' not in item:
-        #    print('got bucke')
-        # path_elements = item.split('/')
-
-        # contents = item.get('Contents')
-        # for content in contents:
-        #    print('\n')
-        #    print(content)
-    exit(0)
-
-
-def output_s3_info(output_media=None, item=None):
-    """
-    @type output_media: unicode
-    @type item: unicode
-    """
-    # if output_media == 'console':
-    #    td = list()
-    #    td.append(['id', zone['HostedZone']['Id']])
-    #    td.append(['Name', zone['HostedZone']['Name']])
-    #    td.append(['Count', str(zone['HostedZone']['ResourceRecordSetCount'])])
-    #    td.append(['Comment', zone['HostedZone']['Config']['Comment']])
-    #    td.append(['Private', str(zone['HostedZone']['Config']['PrivateZone'])])
-    #    td.append(['Name Servers', "\n".join(zone['DelegationSet']['NameServers'])])
-    #    td.append(['Records', ' '])
-    #    td.append(['{0}'.format("-" * 12), '{0}'.format("-" * 20)])
-    #    for record_set in record_sets['ResourceRecordSets']:
-    #        td.append(['Name', record_set['Name']])
-    #        td.append([' Type', record_set['Type']])
-    #        td.append([' TTL', str(record_set['TTL'])])
-    #        td.append([' Values', "\n".join(get_record_set_values(record_set['ResourceRecords']))])
-    #    output_ascii_table(table_title="Zone Info",
-    #                       table_data=td)
     exit(0)
--- lib/acli/services/s3.py
+++ lib/acli/services/s3.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import (absolute_import, print_function, unicode_literals)
-from acli.output.s3 import (output_s3_list, output_s3_info)
+from acli.output.s3 import (output_s3_list)
 import botocore.exceptions
 from acli.connections import get_client
 
@@ -11,101 +11,39 @@ def s3_list(aws_config=None, item=None):
     """
     s3_client = get_client(client_type='s3', config=aws_config)
     buckets = s3_client.list_buckets()
-    # NO ITEM, SO TRY TO LIST BUCKETS
     if not item:
         if buckets.get('Buckets', None):
             output_s3_list(output_media='console', buckets=buckets.get('Buckets'))
         else:
             exit("No buckets found.")
     else:
-        # ITEM PROVIDED SO EXTRACT BUCKET NAME, KEY AND THEN OUTPUT
-        print("GOT AN ITEM")
-        bucket_name = None
         prefix = ''
         if item and '/' in item:
-            print('Got fully qualified path')
             path_elements = item.split('/')
-            print('path elements: {}'.format(path_elements))
             bucket_name = path_elements[0]
-            request_key = path_elements[1:]
-            print('request_key:' + str(request_key))
-            print('GOT AN ITEM AND BUCKET NAME = {}'.format(bucket_name))
             prefix = "/".join(path_elements[1:])
-            print('PREFIX: {}'.format(prefix))
         else:
             bucket_name = item
-        print("BNAME" + bucket_name)
-
-        print('prefix = {}'.format(prefix))
         try:
             s3_client.head_bucket(Bucket=bucket_name)
         except botocore.exceptions.ClientError:
             exit("Unable to access bucket.")
         except Exception as unhandled:
             exit('Unhandled exception: {0}'.format(unhandled))
-        # BUCKET DOES EXIST, SO GET CONTENTS
         try:
             objects = s3_client.list_objects(Bucket=bucket_name, Prefix=prefix, Delimiter='/')
             if not objects:
                 exit('nothing there')
-            print("OBJECT = ".format(str(objects)))
             common_prefixes = objects.get('CommonPrefixes', list())
             folders = list()
             for first_bit in common_prefixes:
                 folders.append(first_bit)
-            print(folders)
-            if objects.get('Contents'):
-                for content in objects.get('Contents'):
-                    print("content = " + str(content.get('Key')))
-            #print(objects.get('Contents', None))
             if objects:
-                print(objects)
                 output_s3_list(objects=objects, folders=folders, item=item, bucket_name=bucket_name)
             else:
                 print("NO OBJECTS")
-            # objects = s3_client.list_objects(Bucket=bucket_name, Prefix=prefix)
-            # paginator = s3_client.get_paginator('list_objects')
-            # for result in paginator.paginate(Bucket=bucket_name, Delimiter='/', Prefix=prefix):
-            #    print("Requested top name: {0}".format(result.get('Name')))
-            #    for prefix1 in result.get('CommonPrefixes'):
-            #        print(prefix1)
-                    # for sub in paginator.paginate(Bucket=bucket_name, Delimiter='/', Prefix='/'+prefix1.get('Prefix')):
-                    #    print("child of requested: {0}".format(sub.get('Name')))
         except botocore.exceptions.ClientError as error:
             if 'NoSuchBucket' in error.response['Error']['Code']:
                 exit('Bucket not found.')
             else:
                 exit('Unhandled error: {0}'.format(error.response['Error']['Code']))
-        # output_s3_list(output_media='console', bucket_name=bucket_name, objects=objects)
-        #elif '/' not in item:
-        #    print('Got potential bucket')
-        #    print(buckets.get('Buckets'))
-        #    bucket = [bucket for bucket in buckets.get('Buckets') if bucket.get('Name') == item]
-        #    if bucket:
-        #        print('bucket = {}'.format(bucket))
-        #        output_s3_list(output_media='console', buckets=bucket)
-        #    else:
-        #        exit('Bucket does not exist.')
-        #print("{0} - {1}".format(bucket_name, prefix))
-            #if item in buckets.get('Buckets')
-            #output_s3_list(output_media='console', item=item)
-
-
-def s3_info(aws_config=None, item=None):
-    """
-    @type aws_config: Config
-    @type item: unicode
-    """
-    s3_client = get_client(client_type='ec2', config=aws_config)
-    print(s3_client)
-    #try:
-    #    hosted_zone = s3_client.get_hosted_zone(Id=zone_id)
-    #    record_sets = s3_client.list_resource_record_sets(HostedZoneId=zone_id)
-    #    if hosted_zone['HostedZone']['Id']:
-    #        output_route53_info(output_media='console',
-    #                            zone=hosted_zone,
-    #                            record_sets=record_sets)
-    #except AttributeError:
-    #    exit("Cannot find hosted zone: {0}".format(zone_id))
-    #except botocore.exceptions.ClientError:
-    #    exit("Cannot request hosted zone: {0}".format(zone_id))
