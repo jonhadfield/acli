071f6a591a984467aade02ef8479ecd6a02954ef
Jon Hadfield <jon@lessknown.co.uk>
2015-10-24 15:19:26 +0100

    add first asg test.

---
+++ tests/test_asg.py
@@ -0,0 +1,70 @@
+from __future__ import (absolute_import, print_function, unicode_literals)
+from acli.services.asg import (asg_list, lc_list)
+from acli.config import Config
+from moto import mock_ec2, mock_autoscaling
+
+import pytest
+from boto3.session import Session
+session = Session(region_name="eu-west-1")
+
+
+@pytest.yield_fixture(scope='function')
+def fake_setup():
+    """ASG mock service"""
+    mock = mock_autoscaling()
+    mock.start()
+    client = session.client('autoscaling')
+    client.create_launch_configuration(
+        LaunchConfigurationName='test_lc',
+        ImageId='ami-abcd1234',
+        InstanceType='t2.medium'
+    )
+
+#    client.create_auto_scaling_group(
+#        AutoScalingGroupName='test_asg',
+#        LaunchConfigurationName='test_lc',
+#        InstanceId='string',
+#        MinSize=123,
+#        MaxSize=123,
+#        DesiredCapacity=123,
+#        DefaultCooldown=123,
+#        AvailabilityZones=[
+#            'string',
+#        ],
+#        LoadBalancerNames=[
+#            'string',
+#        ],
+#        HealthCheckType='string',
+#        HealthCheckGracePeriod=123,
+#        PlacementGroup='string',
+#        VPCZoneIdentifier='string',
+#        TerminationPolicies=[
+#            'string',
+#        ],
+#        Tags=[
+#            {
+#                'ResourceId': 'string',
+#                'ResourceType': 'string',
+#                'Key': 'string',
+#                'Value': 'string',
+#                'PropagateAtLaunch': True
+#            },
+#        ]
+#    )
+    yield client.describe_auto_scaling_groups()
+    mock.stop()
+
+
+config = Config(cli_args={'--region': 'eu-west-1',
+                          '--access_key_id': 'AKIAIOSFODNN7EXAMPLE',
+                          '--secret_access_key': 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'})
+
+
+def test_lc_list_service(fake_setup):
+    with pytest.raises(SystemExit):
+        assert lc_list(aws_config=config)
+
+
+# def test_elb_info_service(route53_zone):
+#    with pytest.raises(SystemExit):
+#        assert route53_info(aws_config=config, zone_id=route53_zone.get('Id'))
