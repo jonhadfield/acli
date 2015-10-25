f0c7551aa6f34e9d50266405fd2dc961520023b4
Jon Hadfield <jon@lessknown.co.uk>
2015-10-25 10:24:13 +0000

    improve output.

--- lib/acli/output/route53.py
+++ lib/acli/output/route53.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import (absolute_import, print_function, unicode_literals)
 from acli.output import (output_ascii_table, dash_if_none)
+from colorclass import Color, Windows
+Windows.enable(auto_colors=True, reset_atexit=True)
 
 
 def output_route53_list(output_media=None, zones=None):
@@ -13,9 +15,9 @@ def output_route53_list(output_media=None, zones=None):
     for hosted_zone_dict in zones:
             if output_media == 'console':
                 td = list()
-                td.append(['id', 'name',
-                           'count', 'comment',
-                           'private zone'])
+                td.append([Color('{autoblue}id{/autoblue}'), Color('{autoblue}name{/autoblue}'),
+                           Color('{autoblue}count{/autoblue}'), Color('{autoblue}comment{/autoblue}'),
+                           Color('{autoblue}private zone{/autoblue}')])
                 hosted_zones = sorted(hosted_zone_dict.get('HostedZones'), key=lambda k: k['Name'])
                 for hosted_zone in hosted_zones:
                     zone_id = dash_if_none(hosted_zone.get('Id'))
@@ -28,7 +30,7 @@ def output_route53_list(output_media=None, zones=None):
                                record_count,
                                comment,
                                private_zone])
-                output_ascii_table(table_title="Route53 Zones",
+                output_ascii_table(table_title=Color('{autowhite}route53 zones{/autowhite}'),
                                    table_data=td,
                                    inner_heading_row_border=True)
     exit(0)
@@ -52,19 +54,23 @@ def output_route53_info(output_media=None, zone=None, record_sets=None):
     """
     if output_media == 'console':
         td = list()
-        td.append(['id', zone['HostedZone']['Id']])
-        td.append(['Name', zone['HostedZone']['Name']])
-        td.append(['Count', str(zone['HostedZone']['ResourceRecordSetCount'])])
-        td.append(['Comment', zone['HostedZone']['Config']['Comment']])
-        td.append(['Private', str(zone['HostedZone']['Config']['PrivateZone'])])
-        td.append(['Name Servers', "\n".join(zone['DelegationSet']['NameServers'])])
-        td.append(['Records', ' '])
+        td.append([Color('{autoblue}id{/autoblue}'), zone['HostedZone']['Id']])
+        td.append([Color('{autoblue}name{/autoblue}'), zone['HostedZone']['Name']])
+        td.append([Color('{autoblue}records{/autoblue}'), str(zone['HostedZone']['ResourceRecordSetCount'])])
+        td.append([Color('{autoblue}comment{/autoblue}'), zone['HostedZone']['Config']['Comment']])
+        td.append([Color('{autoblue}private{/autoblue}'), str(zone['HostedZone']['Config']['PrivateZone'])])
+        td.append([Color('{autoblue}name servers{/autoblue}'), "\n".join(zone['DelegationSet']['NameServers'])])
+        td.append([Color('{autoblue}records{/autoblue}'), ' '])
         td.append(['{0}'.format("-" * 12), '{0}'.format("-" * 20)])
         for record_set in record_sets['ResourceRecordSets']:
-            td.append(['Name', record_set['Name']])
-            td.append([' Type', record_set['Type']])
-            td.append([' TTL', str(record_set['TTL'])])
-            td.append([' Values', "\n".join(get_record_set_values(record_set['ResourceRecords']))])
-        output_ascii_table(table_title="Zone Info",
+            td.append([Color('{autoblue}name{/autoblue}'),
+                       record_set['Name']])
+            td.append([Color('{autoblue} type{/autoblue}'),
+                       record_set['Type']])
+            td.append([Color('{autoblue} ttl{/autoblue}'),
+                       str(record_set['TTL'])])
+            td.append([Color('{autoblue} values{/autoblue}'),
+                       "\n".join(get_record_set_values(record_set['ResourceRecords']))])
+        output_ascii_table(table_title=Color('{autowhite}zone info{/autowhite}'),
                            table_data=td)
     exit(0)
