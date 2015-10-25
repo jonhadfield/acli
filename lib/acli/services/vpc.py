693cfc1e0d8c75da9ccc5dfd5d18dfe9317a79db
Jon Hadfield <jon@lessknown.co.uk>
2015-10-25 10:12:58 +0000

    handle missing VPC.

--- lib/acli/services/vpc.py
+++ lib/acli/services/vpc.py
@@ -2,6 +2,7 @@
 from __future__ import (absolute_import, print_function, unicode_literals)
 from acli.output.vpc import (output_vpc_list, output_vpc_info)
 from acli.connections import get_client
+from botocore.exceptions import ClientError
 
 
 def vpc_list(aws_config=None):
@@ -23,10 +24,12 @@ def vpc_info(aws_config=None, vpc_id=None):
     """
     ec2_client = get_client(client_type='ec2', config=aws_config)
 
-    vpcs = ec2_client.describe_vpcs(VpcIds=[vpc_id])
+    try:
+        vpcs = ec2_client.describe_vpcs(VpcIds=[vpc_id])
+    except (ClientError, IndexError):
+        exit("Cannot find VPC: {0}".format(vpc_id))
 
     if vpcs.get('Vpcs', None):
         all_subnets = ec2_client.describe_subnets(Filters=[{'Name': 'vpc-id', 'Values': [vpc_id]}])
         output_vpc_info(output_media='console', vpc=vpcs['Vpcs'][0], subnets=all_subnets)
-    else:
-        exit("Cannot find VPC: {0}.".format(vpc_id))
+
