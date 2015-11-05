ab9e64a5417ad81d181c75531545fcf16150deae
Jon Hadfield <jon@lessknown.co.uk>
2015-11-05 20:27:19 +0000

    show desired on asg list.

--- lib/acli/output/asg.py
+++ lib/acli/output/asg.py
@@ -24,11 +24,12 @@ def output_asg_list(output_media=None, asg_list=None):
     if isinstance(asg_list, list):
         if output_media == 'console':
             td = [[Color('{autoblue}name{/autoblue}'), Color('{autoblue}instances{/autoblue}'),
-                   Color('{autoblue}min{/autoblue}'), Color('{autoblue}max{/autoblue}'),
+                   Color('{autoblue}desired{/autoblue}'), Color('{autoblue}min{/autoblue}'), Color('{autoblue}max{/autoblue}'),
                    Color('{autoblue}lc{/autoblue}')]]
             for asg in asg_list:
                 td.append([asg.get('AutoScalingGroupName', '-'),
                            str(len(asg.get('Instances', '-'))),
+                           str(asg.get('DesiredCapacity', '-')),
                            str(asg.get('MinSize', '-')),
                            str(asg.get('MaxSize', '-')),
                            asg.get('LaunchConfigurationName', '-')
