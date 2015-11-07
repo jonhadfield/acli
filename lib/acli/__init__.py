e244e27c3f62e26aa1a614b81293b1b3b033af4b
Jon Hadfield <jon@lessknown.co.uk>
2015-11-07 15:34:26 +0000

    bump version.

--- lib/acli/__init__.py
+++ lib/acli/__init__.py
@@ -49,7 +49,7 @@ from acli.utils import install_completion
 
 def real_main():
     args = docopt(__doc__,
-                  version='0.1.6',
+                  version='0.1.7',
                   options_first=True)
     aws_config = Config(args)
     argv = [args['<command>']] + args['<args>']
