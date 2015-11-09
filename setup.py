0c7755cc0933e8860a1e23ac6d0d5e35ac67a520
Jon Hadfield <jon@lessknown.co.uk>
2015-11-09 09:45:10 +0000

    default to console if not supplied. fixes empty listing and info for amis.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 sys.path.insert(0, os.path.abspath('lib'))
 
-version = "0.1.9"
+version = "0.1.10"
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
