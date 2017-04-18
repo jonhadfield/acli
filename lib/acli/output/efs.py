ab4e2378e6d8f1271e5c297471252eae1391b3ab
Jon Hadfield <jon@lessknown.co.uk>
2017-04-18 11:13:18 +0100

    add efs info and bump version.

--- lib/acli/output/efs.py
+++ lib/acli/output/efs.py
@@ -3,9 +3,9 @@ from __future__ import (absolute_import, print_function, unicode_literals)
 
 import humanize
 from colorclass import Color, Windows
-from external.six import iteritems
 
 from acli.output import (output_ascii_table, output_ascii_table_list, dash_if_none)
+from external.six import iteritems
 
 Windows.enable(auto_colors=True, reset_atexit=True)
 
@@ -28,20 +28,20 @@ def colour_state(state=None):
         return Color('{autoyellow}' + state + '{/autoyellow}')
 
 
-def output_filesystems(filesystems=None):
+def output_filesystems(filesystems=None, mount_targets=None):
     """
     @type filesystems: dict
+    @type mount_targets: list
     """
     td = list()
     table_header = [Color('{autoblue}id{/autoblue}'),
-                    Color('{autoblue}owner id{/autoblue}'),
                     Color('{autoblue}name{/autoblue}'),
                     Color('{autoblue}state{/autoblue}'),
                     Color('{autoblue}size / time (UTC){/autoblue}'),
                     Color('{autoblue}mode{/autoblue}'),
-                    Color('{autoblue}mount targets{/autoblue}'),
-                    Color('{autoblue}created (UTC){/autoblue}')
+                    Color('{autoblue}mount targets{/autoblue}')
                     ]
+    fs_ids = list()
     for fs in filesystems:
         size_in_bytes = fs.get('SizeInBytes')
         size_in_bytes_value = size_in_bytes.get('Value')
@@ -55,16 +55,30 @@ def output_filesystems(filesystems=None):
             created_time = created_time.replace(tzinfo=None, second=0)
 
         td.append([fs.get('FileSystemId'),
-                   fs.get('OwnerId'),
-                   fs.get('Name'),
+                   dash_if_none(fs.get('Name')),
                    colour_state(fs.get('LifeCycleState')),
                    '{0} / {1}'.format(size_in_bytes_value,
                                       dash_if_none(size_in_bytes_timestamp)),
                    fs.get('PerformanceMode'),
-                   fs.get('NumberOfMountTargets'),
-                   dash_if_none(created_time)
+                   dash_if_none(fs.get('NumberOfMountTargets'))
                    ])
-    output_ascii_table_list(table_title=Color('{autowhite}EFS{/autowhite}'),
+        fs_ids.append(fs.get('FileSystemId'))
+    output_ascii_table_list(table_title=Color('{autowhite}EFS Filesystems{/autowhite}'),
+                            table_data=td,
+                            table_header=table_header,
+                            inner_heading_row_border=True)
+    # Output mount targets
+    td = list()
+    table_header = [Color('{autoblue}mount target id{/autoblue}'),
+                    Color('{autoblue}filesystem id{/autoblue}'),
+                    Color('{autoblue}lifecycle state{/autoblue}')
+                    ]
+    for mt in mount_targets:
+        td.append([mt.get('MountTargetId'),
+                   mt.get('FileSystemId'),
+                   colour_state(mt.get('LifeCycleState'))
+                   ])
+    output_ascii_table_list(table_title=Color('{autowhite}EFS Mount Targets{/autowhite}'),
                             table_data=td,
                             table_header=table_header,
                             inner_heading_row_border=True)
@@ -102,44 +116,58 @@ def colour_processing(state=None):
         return Color('{autoyellow}True{/autoyellow}')
 
 
-def output_filesystem_info(domain=None):
+def output_filesystem_info(filesystem=None, mount_targets=None):
     """
-    @type domain: dict
+    @type filesystem: dict
+    @type mount_targets: list
     """
-    if domain:
-        domain_details = domain.get('DomainStatusList')[0]
-        cluster_conf = domain_details.get('ElasticsearchClusterConfig')
+    if filesystem:
+        filesystem_details = filesystem.get('FileSystems')[0]
         td = list()
-        td.append([Color('{autoblue}domain name{/autoblue}'),
-                   domain_details.get('DomainName')])
-        td.append([Color('{autoblue}endpoint{/autoblue}'),
-                   domain_details.get('Endpoint')])
-        td.append([Color('{autoblue}created{/autoblue}'),
-                   colour_created(domain_details.get('Created'))])
-        td.append([Color('{autoblue}deleted{/autoblue}'),
-                   colour_deleted(domain_details.get('Deleted'))])
-        td.append([Color('{autoblue}processing{/autoblue}'),
-                   colour_processing(domain_details.get('Processing'))])
-        td.append([Color('{autoblue}cluster config{/autoblue}'),
-                   ' '])
-        td.append([Color('{autoblue} dedicated master enabled{/autoblue}'),
-                   str(cluster_conf.get('DedicatedMasterEnabled'))])
-        td.append([Color('{autoblue} instance type{/autoblue}'),
-                   str(cluster_conf.get('InstanceType'))])
-        td.append([Color('{autoblue} instance count{/autoblue}'),
-                   str(cluster_conf.get('InstanceCount'))])
-        td.append([Color('{autoblue} zone awareness{/autoblue}'),
-                   str(cluster_conf.get('ZoneAwarenessEnabled'))])
-        td.append([Color('{autoblue}domain id{/autoblue}'),
-                   domain_details.get('DomainId')])
-        td.append([Color('{autoblue}snapshot options{/autoblue}'),
-                   output_dict(domain_details.get('SnapshotOptions'))])
-        td.append([Color('{autoblue}advanced options{/autoblue}'),
-                   output_dict(domain_details.get('AdvancedOptions'))])
-        td.append([Color('{autoblue}ARN{/autoblue}'),
-                   domain_details.get('ARN')])
-        output_ascii_table(table_title=Color('{autowhite}ES domain info{/autowhite}'),
+        td.append([Color('{autoblue}filesystem name{/autoblue}'),
+                   dash_if_none(filesystem_details.get('Name'))])
+        td.append([Color('{autoblue}id{/autoblue}'),
+                   filesystem_details.get('FileSystemId')])
+        td.append([Color('{autoblue}size{/autoblue}'),
+                   filesystem_details['SizeInBytes']['Value']])
+        td.append([Color('{autoblue}owner id{/autoblue}'),
+                   dash_if_none(filesystem_details.get('OwnerId'))])
+        td.append([Color('{autoblue}creation time{/autoblue}'),
+                   dash_if_none(filesystem_details.get('CreationTime'))])
+        td.append([Color('{autoblue}lifecycle state{/autoblue}'),
+                   dash_if_none(filesystem_details.get('LifeCycleState'))])
+        td.append([Color('{autoblue}no. mount targets{/autoblue}'),
+                   dash_if_none(filesystem_details.get('NumberOfMountTargets'))])
+        td.append([Color('{autoblue}performance mode{/autoblue}'),
+                   dash_if_none(filesystem_details.get('PerformanceMode'))])
+        td.append([Color('{autoblue}creation token{/autoblue}'),
+                   dash_if_none(filesystem_details.get('CreationToken'))])
+        output_ascii_table(table_title=Color('{autowhite}EFS filesystem info{/autowhite}'),
                            table_data=td)
     else:
-        exit('Domain does not exist.')
+        exit('filesystem does not exist.')
+
+    if mount_targets:
+        table_header = [Color('{autoblue}mount target id{/autoblue}'),
+                        Color('{autoblue}filesystem id{/autoblue}'),
+                        Color('{autoblue}lifecycle state{/autoblue}'),
+                        Color('{autoblue}ip address{/autoblue}'),
+                        Color('{autoblue}subnet id{/autoblue}'),
+                        Color('{autoblue}interface id{/autoblue}'),
+                        Color('{autoblue}owner id{/autoblue}')
+                        ]
+        td = list()
+        for mt in mount_targets:
+            td.append([mt.get('MountTargetId'),
+                       mt.get('FileSystemId'),
+                       colour_state(mt.get('LifeCycleState')),
+                       mt.get('IpAddress'),
+                       mt.get('SubnetId'),
+                       mt.get('NetworkInterfaceId'),
+                       mt.get('OwnerId')
+                       ])
+        output_ascii_table_list(table_title=Color('{autowhite}EFS Mount Targets{/autowhite}'),
+                                table_header=table_header,
+                                table_data=td,
+                                inner_heading_row_border=True)
     exit(0)
